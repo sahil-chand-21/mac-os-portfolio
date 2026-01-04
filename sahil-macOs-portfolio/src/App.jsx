4355527a6caf1673e29f6a669f54a926b3eb4a8a
sahil-chand-21 <sahil.chand79065@gmail.com>
2026-01-05 00:01:12 +0530

    making navbar and constants are left (migrating all data into constant file)for clean code

---
+++ sahil-macOs-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '#components/Navbar'
+const App = () => {
+  return (
+    <main>
+    <Navbar/>
+    <div> MY portfolio</div>
+    </main>
+  )
+}
+
+export default App
---
+++ sahil-macOs-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Sahil-portfolio</p>
+    </nav>
+
+  )
+}
+
+export default Navbar
