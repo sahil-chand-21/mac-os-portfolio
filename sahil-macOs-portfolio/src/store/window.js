5bc528797ebae6c4a16a8611b507532bfe1c3354
sahil-chand-21 <sahil.chand79065@gmail.com>
2026-01-07 17:02:19 +0530

    window working

--- sahil-macOs-portfolio/src/store/window.js
+++ sahil-macOs-portfolio/src/store/window.js
@@ -1,5 +1,16 @@
+import { INITIAL_Z_INDEX, WINDOW_CONFIG } from "#constants";
 import { create } from "zustand";
 
-const useWindowStore= create((set)=>{
+const useWindowStore= create(immer((set)=>({
+    windows: WINDOW_CONFIG,
+    nextZIndex:INITIAL_Z_INDEX + 1,
 
-});
+    openWindow:(windowKey,data=null)=>set((state)=>{
+        const win =state.windows[windowKey];
+        win.isOpen = true;
+        win.zIndex = INITIAL_Z_INDEX;
+        win.data= data ?? win.data;
+        state.nextZIndex++;
+    })
+
+})));
